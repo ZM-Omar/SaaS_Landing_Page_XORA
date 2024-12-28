18aa7c53bf55a396d3e82e4ebb5d7e9f6cd2284f
ZM-Omar <l21212374@tectijuana.edu.mx>
2024-12-27 23:57:43 -0800

    refactor: update Button component and enhance feature details

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,24 +1,46 @@
 export const features = [
-    {
-        id: '0',
-        icon: '/images/feature-1.svg',
-        caption: 'DEMO CAPTION',
-        title: 'FEATURE 1',
-        text: 'TEXT 1',
-        button:{
-            icon: '/images/magictouch.svg',
-            text: 'Watch the demo'
-        }
+  {
+    id: '0',
+    icon: '/images/feature-1.png',
+    caption: 'Easy integration',
+    title: 'Work smarter not harder',
+    text: "With Xora, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
+    button: {
+      icon: '/images/magictouch.svg',
+      title: 'Watch the demo',
     },
-        {
-        id: '1',
-        icon: '/images/feature-2.svg',
-        caption: 'DEMO CAPTION',
-        title: 'FEATURE 2',
-        text: 'TEXT 2',
-        button:{
-            icon: '/images/magictouch.svg',
-            text: 'Watch the demo'
-        }
-    }
-]
+  },
+  {
+    id: '1',
+    icon: '/images/feature-2.png',
+    caption: 'Secure & trustworthy',
+    title: 'Sleep easy, we got your back',
+    text: 'Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Xora helps keeps your information secure and locked up tighter than Fort Knox.',
+    button: {
+      icon: '/images/docs.svg',
+      title: 'Read the docs',
+    },
+  },
+];
+export const details = [
+  {
+    id: '0',
+    icon: '/images/detail-1.png',
+    title: 'AI automated video editing',
+  },
+  {
+    id: '1',
+    icon: '/images/detail-2.png',
+    title: 'Collaborate with your team',
+  },
+  {
+    id: '2',
+    icon: '/images/detail-3.png',
+    title: 'Ultra fast cloud-engine',
+  },
+  {
+    id: '3',
+    icon: '/images/detail-4.png',
+    title: '24 / 7 Customer support',
+  },
+];
