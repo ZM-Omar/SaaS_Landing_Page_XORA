d0a6a9b8e23e65c227c978bba9f8a70ff06f1e21
ZM-Omar <l21212374@tectijuana.edu.mx>
2024-12-27 22:44:39 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Features from "./sections/Features.jsx";
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export const features = [
+    {
+        id: '0',
+        icon: '/images/feature-1.svg',
+        caption: 'DEMO CAPTION',
+        title: 'FEATURE 1',
+        text: 'TEXT 1',
+        button:{
+            icon: '/images/magictouch.svg',
+            text: 'Watch the demo'
+        }
+    },
+        {
+        id: '1',
+        icon: '/images/feature-2.svg',
+        caption: 'DEMO CAPTION',
+        title: 'FEATURE 2',
+        text: 'TEXT 2',
+        button:{
+            icon: '/images/magictouch.svg',
+            text: 'Watch the demo'
+        }
+    }
+]
